d91db44bf96ff9d4030181e39964c823727495ef
Brian Reynolds <bri.reynolds@gmail.com>
2022-12-26 15:39:40 -0600

    :tada: add league-of-assassins lib

---
+++ libs/league-of-assassins/src/lib/league-of-assassins.spec.ts
@@ -0,0 +1,7 @@
+import { leagueOfAssassins } from "./league-of-assassins";
+
+describe("leagueOfAssassins", () => {
+  it("should work", () => {
+    expect(leagueOfAssassins()).toEqual("league-of-assassins");
+  });
+});
