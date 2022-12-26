d96ddb3bed1e3db0240ad6119750251845899603
Brian Reynolds <bri.reynolds@gmail.com>
2022-12-26 15:37:26 -0600

    :tada: add five-player lib

---
+++ libs/five-player/src/lib/five-player.spec.ts
@@ -0,0 +1,7 @@
+import { fivePlayer } from "./five-player";
+
+describe("fivePlayer", () => {
+  it("should work", () => {
+    expect(fivePlayer()).toEqual("five-player");
+  });
+});
---
+++ libs/five-player/src/lib/five-player.ts
@@ -0,0 +1,3 @@
+export function fivePlayer(): string {
+  return "five-player";
+}
