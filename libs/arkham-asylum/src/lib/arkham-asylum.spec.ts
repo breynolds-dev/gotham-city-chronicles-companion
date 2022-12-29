875302672fff12b093db7bda692d08bd2ef68006
Brian Reynolds <bri.reynolds@gmail.com>
2022-12-28 18:10:07 -0600

    :sparkles: feat: add arkham-asylum base files

--- libs/arkham-asylum/src/lib/arkham-asylum.spec.ts
+++
@@ -1,7 +0,0 @@
-import { arkhamAsylum } from "./arkham-asylum";
-
-describe("arkhamAsylum", () => {
-  it("should work", () => {
-    expect(arkhamAsylum()).toEqual("arkham-asylum");
-  });
-});
--- libs/arkham-asylum/src/lib/arkham-asylum.ts
+++
@@ -1,3 +0,0 @@
-export function arkhamAsylum(): string {
-  return "arkham-asylum";
-}
